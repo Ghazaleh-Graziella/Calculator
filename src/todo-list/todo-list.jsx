6537cd1301f756c11666962f38e4ee92e63b1147
Ghazaleh-Graziella <ghazale.f@yahoo.com>
2022-01-06 00:17:59 +0330

    add todoList component +its own component

---
+++ src/todo-list/todo-list.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import "./todo-list.scss";
+import TodoForm from "./todo-form";
+import Todo from "./todo";
+import Modal from "react-modal";
+
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const addTodo = (todo) => {
+    const newTodos = [todo, ...todos];
+
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
+
+    if (newTodos.length !== 0) {
+      let i = 1;
+      while (i < newTodos.length) {
+        if (newTodos[i].text === todo.text) {
+          setModalIsOpen(true);
+        }
+        i++;
+      }
+    }
+    setTodos(newTodos);
+  };
+
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
+    }
+    setTodos((prev) =>
+      prev.map((item) => (item.id === todoId ? newValue : item))
+    );
+  };
+
+  const removeTodo = (id) => {
+    const removeArr = [...todos].filter((todo) => todo.id !== id);
+    setTodos(removeArr);
+  };
+
+  const completeTodo = (id) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div className="todo-app">
+      <div>
+        <h1>What's the task for Today?</h1>
+        <TodoForm onSubmit={addTodo} />
+        <Todo
+          todos={todos}
+          completeTodo={completeTodo}
+          updateTodo={updateTodo}
+          removeTodo={removeTodo}
+        />
+        <Modal />
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
